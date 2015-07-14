ee890fdd2ed63d6bcfdb058a32a71e2136619dc7
shuraic <irving.j.iturbe.cruz@gmail.com>
2015-07-14 16:11:32 -0500

    Create OperationController.java
    OperationController is the controller dedicated to handle the redirect from  the views

---
+++ src/main/java/com/utez/preproyecto/controller/OperationController.java
@@ -0,0 +1,40 @@
+package com.utez.preproyecto.controller;
+
+import com.utez.preproyecto.model.PreProyecto;
+import com.utez.preproyecto.service.PreProyectoService;
+import com.utez.preproyecto.service.CleinteService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Shura
+ */
+
+@Controller
+public class OperationController {
+    
+    @Autowired
+    private PreProyectoService preservice;
+    
+    @Autowired
+    private ClienteServicio clienteservice;
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String goIndex() {
+        return "index";
+    }
+    
+    @RequestMapping(value = "/createpre", method = RequestMethod.GET)
+    public String goCreatePre(Model model) {
+        List<Cliente> clientes = clienteservice.getAllClientes();
+        List<PreProyecto> proyectos = preservice.getAllPreProyecto();
+        model.addAttribute("clientes", clientes);
+        model.addAttribute("proyectos", proyectos);
+        return "createpre";
+    }
+}
