f5bb473e60509e406c4537120703ff48b939fe22
hebertorg <hebertorg@live.com>
2015-07-17 02:12:48 +0200

    Create Cliente.java

---
+++ src/main/java/com/utez/preproyecto/model/Cliente.java
@@ -0,0 +1,97 @@
+package com.example.crud.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Heberto
+ */
+//Se escribe el nombre de la tabla
+@Entity(name = "Cliente")
+public class Cliente {
+   
+    @Id 
+    @Column 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private int id;
+    
+    @Column
+    private String nombre;
+    
+    @Column
+    private String apPaterno;
+    
+    @Column
+    private String apMaterno;
+    
+    @Column
+    private String direccion;
+    
+    @Column
+    private String telefono;
+    
+    @Column
+    private String correo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApPaterno() {
+        return apPaterno;
+    }
+
+    public void setApPaterno(String apPaterno) {
+        this.apPaterno = apPaterno;
+    }
+
+    public String getApMaterno() {
+        return apMaterno;
+    }
+
+    public void setApMaterno(String apMaterno) {
+        this.apMaterno = apMaterno;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
